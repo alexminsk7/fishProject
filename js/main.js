0497797ae2d1f41e1b51a5921993200076225af8
Alex Lisovsky <lisouski87@gmail.com>
2021-12-02 18:56:53 +0300

    feat: finished main screen, added slick slider into the project

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-$(function(){
-
+$(function () {
+	$('.header-slider').slick({
+		dots: true,
+		vertical: true,
+		prevArrow:
+			'<button type="button" class="slick-prev"><img src="./images/svg/arrowUp.svg" alt="arrow up"></button>',
+		nextArrow:
+			'<button type="button" class="slick-next"><img src="./images/svg/arrowDown.svg" alt="arrow down"></button>',
+	});
 });
