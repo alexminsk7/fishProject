7a5bad162ca6a31e221db932f42a3541ef5ab214
Alex Lisovsky <lisouski87@gmail.com>
2021-12-03 22:21:00 +0300

    feat: finished main section, added second slider

--- js/main.js
+++ js/main.js
@@ -7,4 +7,25 @@ $(function () {
 		nextArrow:
 			'<button type="button" class="slick-next"><img src="./images/svg/arrowDown.svg" alt="arrow down"></button>',
 	});
+
+	$('.product__name').slick({
+		slidesToShow: 4.5,
+		slidesToScroll: 1,
+		focusOnSelect: true,
+		asNavFor: '.product__content',
+		vertical: true,
+		centerMode: true,
+		prevArrow:
+			'<button type="button" class="product-prev"><img src="./images/svg/arrowUpNavy.svg" alt="arrow up"></button>',
+		nextArrow:
+			'<button type="button" class="product-next"><img src="./images/svg/arrowDownNavy.svg" alt="arrow down"></button>',
+	});
+
+	$('.product__content').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		asNavFor: '.product__name',
+		fade: true,
+		arrows: false,
+	});
 });
