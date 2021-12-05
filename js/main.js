baba7859a691fe1dc40ec03bfff8f3f0ff6e324d
Alex Lisovsky <lisouski87@gmail.com>
2021-12-05 10:56:09 +0300

    feat: finished responsive for training project

--- js/main.js
+++ js/main.js
@@ -6,6 +6,17 @@ $(function () {
 			'<button type="button" class="slick-prev"><img src="./images/svg/arrowUp.svg" alt="arrow up"></button>',
 		nextArrow:
 			'<button type="button" class="slick-next"><img src="./images/svg/arrowDown.svg" alt="arrow down"></button>',
+		responsive: [
+			{
+				breakpoint: 361,
+				settings: {
+					dots: false,
+					arrows: false,
+					autoplay: true,
+					autoplaySpeed: 4000,
+				},
+			},
+		],
 	});
 
 	$('.product__name').slick({
@@ -19,6 +30,24 @@ $(function () {
 			'<button type="button" class="product-prev"><img src="./images/svg/arrowUpNavy.svg" alt="arrow up"></button>',
 		nextArrow:
 			'<button type="button" class="product-next"><img src="./images/svg/arrowDownNavy.svg" alt="arrow down"></button>',
+		responsive: [
+			{
+				breakpoint: 1025,
+				settings: {
+					arrows: false,
+					vertical: false,
+					slidesToShow: 3,
+					dots: true,
+				},
+				breakpoint: 767,
+				settings: {
+					arrows: false,
+					vertical: false,
+					slidesToShow: 1,
+					dots: true,
+				},
+			},
+		],
 	});
 
 	$('.product__content').slick({
@@ -27,5 +56,9 @@ $(function () {
 		asNavFor: '.product__name',
 		fade: true,
 		arrows: false,
+	});
+
+	$('.menu__btn').on('click', function () {
+		$('.menu__list').toggleClass('menu__list--active');
 	});
 });
